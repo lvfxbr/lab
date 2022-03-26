f1b9f5c59e9c514383435560a19a2fbd2f5fa6fd
Lucas Vendramini <98402608+lvfxbr@users.noreply.github.com>
2022-03-25 21:36:53 -0300

    Basic working example
    Based in the simple sample of the repository of next-i18next, tested and working as intended.

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import { useTranslation } from "next-i18next";
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    const { t } = useTranslation("common");
+
+    const router = useRouter();
+
+    return (
+        <div className={styles.container}>
+            <p>Current locale: {router.locale}</p>
+            <p>{t("message")}</p>
+            <Link href="/" locale={router.locale === "ptbr" ? "en" : "ptbr"}>
+                <button>Switch locale</button>
+            </Link>
+        </div>
+    );
+}
+
+export const getStaticProps = async ({ locale }) => {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common"])),
+        },
+    };
+};
