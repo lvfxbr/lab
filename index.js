b818575570697f3e4f8451a4bed6cd301ed7c0ab
Lucas Vendramini <98402608+lvfxbr@users.noreply.github.com>
2022-04-19 10:12:34 -0300

    Initial state
    In this version, sync-directory is version 4.0.12, that could have some bugs related by @hoperyy.

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const syncDirectory = require("sync-directory");
+const duration = 60 * 1000;
+
+function doSync() {
+
+    console.log('Do sync...');
+
+    syncDirectory.sync(path.resolve("a"), path.resolve("b"), {
+        deleteOrphaned: true,
+        exclude: [".git", "node_modules"],
+        chokidarWatchOptions: {
+            persistent: false,
+        },
+        type: "copy",
+        onError(err) {
+            throw err;
+        },
+    });
+
+    console.log('Sync done!');
+}
+
+doSync();
+
+setInterval(() => {
+    doSync();
+}, duration);
